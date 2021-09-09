1ec00a279b2e7cf525221e62ee094432a2872e80
PatrickDowning <56053010+PatrickDowning@users.noreply.github.com>
2021-09-09 10:56:07 -0400

    Create appointmentBook.py
    First attempt at first assignment

---
+++ appointmentBook.py
@@ -0,0 +1,50 @@
+#This class is used to define contact name and telephone number
+class Contact:
+#these are the attributes of the class
+    def person(self,contactName,teleNum):
+        self.contactName= contactName
+        self.teleNum= teleNum
+#these are the two getters for the class
+    def get_name(self):
+        return self.contactName
+    def get_number(self):
+        return self.teleNum   
+#This is the toString for the contact class
+
+
+#this is the abstract class 
+class Event:
+#attribute of Contact class
+
+#attributs for start date/time
+
+
+#This is the toString for the contact class
+
+
+class Appointment():
+#attribute for type of appointment
+
+#type getter for appointment
+    def get_type(self):
+        
+
+#This is the toString for the appointment class
+
+class Meeting():
+#method that adds attende
+
+#getter for attendes
+
+#This is the toString for the meeting class
+
+class appointmentBook:
+
+
+
+
+
+#gets events and dates
+
+class main_methood:
+#tests for methods
